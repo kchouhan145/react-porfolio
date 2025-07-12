c75d38ccc12c7b71970ea510562942c63ef72899
kchouhan145 <k4studies@gmail.com>
2025-07-12 21:30:14 +0530

    added skills and reorder the education

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -7,18 +7,11 @@ const Education = () => {
         <h2 className="text-3xl font-bold mb-8 text-center text-gray-800 dark:text-white">Education</h2>
         <div className="space-y-8">
           <div className="bg-gray-50 border border-gray-300 rounded-lg p-6 shadow-md flex flex-col justify-between transition-shadow duration-300 ease-in-out hover:shadow-lg dark:bg-black dark:shadow-violet-700 dark:border-none">
-            <h3 className="text-xl font-semibold text-green-500 mb-2">High School</h3>
-            <p className="text-gray-700 dark:text-white">Institution: <span className="font-medium">Maharana Pratap HSS Nakur Saharanpur</span></p>
-            <p className="text-gray-700 dark:text-white">Board: <span className="font-medium">Uttar Pradesh Madhyamik Shiksha Parishad</span></p>
-            <p className="text-gray-700 dark:text-white">Year: <span className="font-medium">2019</span></p>
-            <p className="text-gray-700 dark:text-white">Percentage/Grade: <span className="font-medium">84.5%</span></p>
-          </div>
-          <div className="bg-gray-50 border border-gray-300 rounded-lg p-6 shadow-md flex flex-col justify-between transition-shadow duration-300 ease-in-out hover:shadow-lg dark:bg-black dark:shadow-violet-700 dark:border-none">
-            <h3 className="text-xl font-semibold text-green-500 mb-2">Intermediate</h3>
-            <p className="text-gray-700 dark:text-white">Institution: <span className="font-medium">C S Dayanand IC Ambehta Peer Saharanpur</span></p>
-            <p className="text-gray-700 dark:text-white">Board: <span className="font-medium">Uttar Pradesh Madhyamik Shiksha Parishad</span></p>
-            <p className="text-gray-700 dark:text-white">Year: <span className="font-medium">2021</span></p>
-            <p className="text-gray-700 dark:text-white">Percentage/Grade: <span className="font-medium">79%</span></p>
+            <h3 className="text-xl font-semibold text-green-500 mb-2">Post Graduation</h3>
+            <p className="text-gray-700 dark:text-white">Institution: <span className="font-medium">Kurukshetra University , Kurukshetra</span></p>
+            <p className="text-gray-700 dark:text-white">Degree: <span className="font-medium">Masters of Computer Applications</span></p>
+            <p className="text-gray-700 dark:text-white">Year: <span className="font-medium">2026</span></p>
+            <p className="text-gray-700 dark:text-white">Percentage/Grade: <span className="font-medium">Pursuing</span></p>
           </div>
           <div className="bg-gray-50 border border-gray-300 rounded-lg p-6 shadow-md flex flex-col justify-between transition-shadow duration-300 ease-in-out hover:shadow-lg dark:bg-black dark:shadow-violet-700 dark:border-none">
             <h3 className="text-xl font-semibold text-green-500 mb-2">Graduation</h3>
@@ -26,13 +19,6 @@ const Education = () => {
             <p className="text-gray-700 dark:text-white">Degree: <span className="font-medium">Bachelor of Computer Science</span></p>
             <p className="text-gray-700 dark:text-white">Year: <span className="font-medium">2024</span></p>
             <p className="text-gray-700 dark:text-white">Percentage/Grade: <span className="font-medium">8.15</span></p>
-          </div>
-          <div className="bg-gray-50 border border-gray-300 rounded-lg p-6 shadow-md flex flex-col justify-between transition-shadow duration-300 ease-in-out hover:shadow-lg dark:bg-black dark:shadow-violet-700 dark:border-none">
-            <h3 className="text-xl font-semibold text-green-500 mb-2">Post Graduation</h3>
-            <p className="text-gray-700 dark:text-white">Institution: <span className="font-medium">Kurukshetra University , Kurukshetra</span></p>
-            <p className="text-gray-700 dark:text-white">Degree: <span className="font-medium">Masters of Computer Applications</span></p>
-            <p className="text-gray-700 dark:text-white">Year: <span className="font-medium">2026</span></p>
-            <p className="text-gray-700 dark:text-white">Percentage/Grade: <span className="font-medium">Pursuing</span></p>
           </div>
         </div>
       </div>
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -7,50 +7,50 @@ const Skills = () => {
         <h2 className="text-3xl font-bold mb-8 text-center text-gray-400 dark:text-white">My Skills</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
           <div className="flex flex-col items-center bg-gray-50 rounded-lg p-6 shadow hover:shadow-lg transition dark:shadow-violet-700 dark:bg-black dark:text-white">
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="MERN" className="w-12 h-12 mb-4"/>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="MERN" className="w-12 h-12 mb-4" />
             <h3 className="font-semibold text-lg mb-2">MERN Full Stack</h3>
             <p className="text-gray-600 dark:text-white text-center">MongoDB, Express, React, Node.js</p>
           </div>
           <div className="flex flex-col items-center bg-gray-50 rounded-lg p-6 shadow hover:shadow-lg transition dark:shadow-violet-700 dark:bg-black dark:text-white">
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="JavaScript" className="w-12 h-12 mb-4"/>
-            <h3 className="font-semibold text-lg mb-2">JavaScript</h3>
-            <p className="text-gray-600 dark:text-white text-center">ES6+, asynchronous programming, DOM manipulation</p>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" alt="Git & GitHub" className="w-12 h-12 mb-4" />
+            <h3 className="font-semibold text-lg mb-2">Git & GitHub</h3>
+            <p className="text-gray-600 dark:text-white text-center">Version control, collaboration, open source</p>
           </div>
           <div className="flex flex-col items-center bg-gray-50 rounded-lg p-6 shadow hover:shadow-lg transition dark:shadow-violet-700 dark:bg-black dark:text-white">
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg" alt="Tailwind CSS" className="w-12 h-12 mb-4"/>
-            <h3 className="font-semibold text-lg mb-2">Tailwind CSS</h3>
-            <p className="text-gray-600 dark:text-white text-center">Utility-first, responsive design, custom themes</p>
-          </div>
-          <div className="flex flex-col items-center bg-gray-50 rounded-lg p-6 shadow hover:shadow-lg transition dark:shadow-violet-700 dark:bg-black dark:text-white">
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" alt="Node.js" className="w-12 h-12 mb-4"/>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" alt="Node.js" className="w-12 h-12 mb-4" />
             <h3 className="font-semibold text-lg mb-2">Node.js</h3>
             <p className="text-gray-600 dark:text-white text-center">Express, REST APIs, backend development</p>
           </div>
           <div className="flex flex-col items-center bg-gray-50 rounded-lg p-6 shadow hover:shadow-lg transition dark:shadow-violet-700 dark:bg-black dark:text-white">
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" alt="Express.js" className="w-12 h-12 mb-4 bg-white rounded"/>
-            <h3 className="font-semibold text-lg mb-2">Express.js</h3>
-            <p className="text-gray-600 dark:text-white text-center">REST APIs, middleware, backend frameworks</p>
-          </div>
-          <div className="flex flex-col items-center bg-gray-50 rounded-lg p-6 shadow hover:shadow-lg transition dark:shadow-violet-700 dark:bg-black dark:text-white">
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" alt="AI" className="w-12 h-12 mb-4"/>
-            <h3 className="font-semibold text-lg mb-2">AI</h3>
-            <p className="text-gray-600 dark:text-white text-center">Machine Learning, Deep Learning, NLP</p>
-          </div>
-          <div className="flex flex-col items-center bg-gray-50 rounded-lg p-6 shadow hover:shadow-lg transition dark:shadow-violet-700 dark:bg-black dark:text-white">
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" alt="SQL/DBMS" className="w-12 h-12 mb-4"/>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" alt="SQL/DBMS" className="w-12 h-12 mb-4" />
             <h3 className="font-semibold text-lg mb-2">SQL/DBMS</h3>
             <p className="text-gray-600 dark:text-white text-center">MySQL, PostgreSQL, database design, queries</p>
           </div>
           <div className="flex flex-col items-center bg-gray-50 rounded-lg p-6 shadow hover:shadow-lg transition dark:shadow-violet-700 dark:bg-black dark:text-white">
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" alt="Python" className="w-12 h-12 mb-4"/>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg" alt="Tailwind CSS" className="w-12 h-12 mb-4" />
+            <h3 className="font-semibold text-lg mb-2">Tailwind CSS</h3>
+            <p className="text-gray-600 dark:text-white text-center">Utility-first, responsive design, custom themes</p>
+          </div>
+          <div className="flex flex-col items-center bg-gray-50 rounded-lg p-6 shadow hover:shadow-lg transition dark:shadow-violet-700 dark:bg-black dark:text-white">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" alt="Python" className="w-12 h-12 mb-4" />
             <h3 className="font-semibold text-lg mb-2">Python</h3>
             <p className="text-gray-600 dark:text-white text-center">Scripting, automation, data analysis</p>
           </div>
           <div className="flex flex-col items-center bg-gray-50 rounded-lg p-6 shadow hover:shadow-lg transition dark:shadow-violet-700 dark:bg-black dark:text-white">
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg" alt="C++" className="w-12 h-12 mb-4"/>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg" alt="C++" className="w-12 h-12 mb-4" />
             <h3 className="font-semibold text-lg mb-2">C++ Programming</h3>
             <p className="text-gray-600 dark:text-white text-center">OOP, STL, competitive programming</p>
           </div>
+          <div className="flex flex-col items-center bg-gray-50 rounded-lg p-6 shadow hover:shadow-lg transition dark:shadow-violet-700 dark:bg-black dark:text-white">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" alt="Express.js" className="w-12 h-12 mb-4 bg-white rounded" />
+            <h3 className="font-semibold text-lg mb-2">Express.js</h3>
+            <p className="text-gray-600 dark:text-white text-center">REST APIs, middleware, backend frameworks</p>
+          </div>
+        <div className="flex flex-col items-center bg-gray-50 rounded-lg p-6 shadow hover:shadow-lg transition dark:shadow-violet-700 dark:bg-black dark:text-white">
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="JavaScript" className="w-12 h-12 mb-4" />
+          <h3 className="font-semibold text-lg mb-2">JavaScript</h3>
+          <p className="text-gray-600 dark:text-white text-center">ES6+, asynchronous programming, DOM manipulation</p>
+        </div>
         </div>
       </div>
     </div>
