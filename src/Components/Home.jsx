cba8ca058d9a7d9c5cf96db7ea2114ff817a7c32
kchouhan145 <k4studies@gmail.com>
2025-07-30 12:32:54 +0530

    Added the CV Button

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { FaGithub, FaTwitter, FaInstagram, FaLinkedin, FaStackOverflow, FaWhatsapp } from 'react-icons/fa'
+import { FaGithub,  FaInstagram, FaLinkedin,  FaWhatsapp } from 'react-icons/fa'
 import heroImg from '../assets/hero.jpg'
-import CVpdf from '../assets/KARTIK CHOUHAN CV.pdf'
 
 const Home = () => {
   return (
@@ -30,7 +29,7 @@ const Home = () => {
             className="w-48 h-48 md:w-80 md:h-80 object-cover rounded-3xl shadow-lg"
           />
           <a
-            href={CVpdf}
+            href='https://drive.google.com/file/d/17FKGxJKOQ8kGed8JAHpTDPbR_ZHMlIFt/view?usp=sharing'
             target="_blank"
             rel="noopener noreferrer"
             className="mt-6 px-6 py-2 bg-green-500 text-white rounded-lg shadow hover:bg-green-600 transition"
