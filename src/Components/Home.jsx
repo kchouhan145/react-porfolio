24eb65a5ff51e9e44d545fd558847a538410bd1b
kchouhan145 <k4studies@gmail.com>
2026-03-04 02:49:10 +0530

    updated cv

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -33,7 +33,7 @@ const Home = () => {
             className="w-48 h-48 md:w-80 md:h-80 object-cover rounded-3xl shadow-lg"
           />
           <a
-            href='https://drive.google.com/file/d/17FKGxJKOQ8kGed8JAHpTDPbR_ZHMlIFt/view?usp=sharing'
+            href='https://drive.google.com/file/d/1o8hUahsgaW74-urCeTIu7YaBzrnglFZ7/view?usp=sharing'
             target="_blank"
             rel="noopener noreferrer"
             className="mt-6 px-6 py-2 bg-green-500 text-white rounded-lg shadow hover:bg-green-600 transition"
